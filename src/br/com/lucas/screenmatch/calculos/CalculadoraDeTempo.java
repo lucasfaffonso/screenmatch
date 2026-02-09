02f5e4ef630cefdfab9e5a74872dc820b48b9c61
Lucas Affonso <lucasfernandesaffonso@gmail.com>
2026-02-08 23:13:27 -0300

    Adição de ArrayList, toString e ajustes

--- src/br/com/lucas/screenmatch/calculos/CalculadoraDeTempo.java
+++ src/br/com/lucas/screenmatch/calculos/CalculadoraDeTempo.java
@@ -1,14 +1,11 @@
 package br.com.lucas.screenmatch.calculos;
 
+import br.com.lucas.screenmatch.modelos.Filme;
+import br.com.lucas.screenmatch.modelos.Serie;
 import br.com.lucas.screenmatch.modelos.Titulo;
 
 public class CalculadoraDeTempo {
-    private int tempoTotal = 0;
-
-    public void inclui(Titulo titulo) {
-        System.out.println("Adicionando duração em minutos de: " + titulo);
-        this.tempoTotal += titulo.getDuracaoEmMinutos();
-    }
+    private int tempoTotal;
 
     public int getTempoTotal() {
         return this.tempoTotal;
@@ -21,4 +18,9 @@ public class CalculadoraDeTempo {
 //    public void inclui(Serie s) {
 //        this.tempoTotal += s.getDuracaoEmMinutos();
 //    }
+
+    public void inclui(Titulo titulo) {
+        System.out.println("Adicionando duração em minutos de " + titulo);
+        this.tempoTotal += titulo.getDuracaoEmMinutos();
+    }
 }
